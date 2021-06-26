cde2bada9f670ffc3f1f80992013b575b21916ff
Sean Mull <kimlan065@gmail.com>
2021-06-25 22:28:55 -0700

    updateing notes

--- Running_JsScript/script.js
+++ Running_JsScript/script.js
@@ -1,6 +1,13 @@
-let x = Math.random();
+// let x = Math.random();
 
-if(x < .5){
-    console.log("Hello world1");
+// if(x < .5){
+//     console.log("Hello world1");
+// }
+
+const dayOfWeek = 'Tuesday';
+
+if(dayOfWeek === 'Monday'){
+    console.log("hate mondays")
+}else if(dayOfWeek === 'Tuesday'){
+    console.log('not to bad');
 }
-
