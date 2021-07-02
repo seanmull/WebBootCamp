f6352d8219b629b4d341af0f8ce6070f1e2e06d9
Sean Mull <kimlan065@gmail.com>
2021-07-01 22:41:25 -0700

    added todo and some functions

--- Basics.js
+++ Basics.js
@@ -1,3 +1,21 @@
-for(let i = 0; i < 10; i++){
-    console.log(i);
+function add(x,y){
+    return x + y;
+}
+
+// console.log(add(1,2));
+
+function random(start, end){
+}
+    return Math.floor(Math.random() * end) + 1;
+
+// console.log(random(1,5));
+// console.log(random(1,5));
+// console.log(random(1,5));
+// console.log(random(1,5));
+// console.log(random(1,5));
+
+let x = 4;
+
+function printnum(){
+    let x = 3;
 }
--- TODO_list/script.js
+++ TODO_list/script.js
@@ -1,25 +1,23 @@
-let todoList = [];
-
-let input = "";
+const todoList = [];
+let input;
 
 while(true){
-    let input = prompt("Enter a command:");
-    if(input == "quit"){
+    input = prompt("Enter a command:");
+    if(input === "quit"){
+        console.log("You quit the game");
         break;
-    }
-    if(input == "new"){
+    }else if(input === "new"){
         let addedItem = prompt("Enter a item to add to list:");
         todoList.push(addedItem);
         console.log(`Added ${addedItem} to the list`);
-    }
-    if(input == "list"){
+    }else if(input === "list"){
         for(let i = 0; i < todoList.length; i++){
-            console.log(i+1," : ",todoList[i]);
+            let index = i + 1;
+            console.log(`${index} : ${todoList[i]}`);
         }
-    }
-    if(input == "delete"){
+    }else if(input === "delete"){
         let removedItem = prompt("Enter a item to remove from the list:");
-        todoList.splice(removedItem+1, 1);
+        todoList.splice(removedItem-1, 1);
         console.log(`Removed ${removedItem} from the list`);
     }
 }
