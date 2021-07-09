0265aa5287785725049e6764cf60fc9b8369f2d1
seanmull <kimlan065@gmail.com>
2021-07-09 16:50:14 -0700

    Update app.js

--- Using Jscript on web/app.js
+++ Using Jscript on web/app.js
@@ -1,1 +1,12 @@
-console.log("hello2");
+const allimages = document.getElementsByTagName('img');
+
+// for(let img of allimages){
+//     //console.log(img.src);
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+const allpara = document.getElementsByTagName('p');
+
+for(let para of allpara){
+    console.log(para.innerHTML);
+}
