6ee5ea3b5d211e0ce3220f4f5fdcaaa29ce2494c
Sean Mull <kimlan065@gmail.com>
2021-07-08 21:13:10 -0700

    commit

--- Basics.js
+++ Basics.js
@@ -212,4 +212,17 @@
 
 // console.log(high,mid,lo);
 
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+//   };
+  
+//   // Only change code below this line 
+//   const half = ({max,min}) => {(max+min)/2};
+//   // Only change code above this line
 
+//   console.log(half(stats));
